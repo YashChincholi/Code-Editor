64326392161fd2308faf03a43b9ea567aaaf015d
YashChincholi <145993625+YashChincholi@users.noreply.github.com>
2024-04-11 21:20:56 +0530

    Created Codepen clone

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header.jsx";
+import Code from "./Code.jsx";
+import Result from "./Result.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Code />
+      <Result />
+    </>
+  );
+};
+
+export default Home;
